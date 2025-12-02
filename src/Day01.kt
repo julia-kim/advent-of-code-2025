ff9d67c8cc0745fc315f97443d45f546f2d191e8
Julia Kim <jkim@messerfinancial.com>
2025-12-02 16:10:13 -0500

    uno

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,70 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var zeroCount = 0
+        input.forEach { rotation ->
+            val direction = rotation.take(1)
+            var distance = rotation.drop(1).toInt()
+            while (distance > 0) {
+                if (dial == 0 && direction == "L") {
+                    dial = 99
+                    distance--
+                    continue
+                }
+                if (dial == 99 && direction == "R") {
+                    dial = 0
+                    distance--
+                    continue
+                }
+                if (direction == "R") {
+                    dial += 1
+                } else {
+                    dial -= 1
+                }
+                distance--
+            }
+            if (dial == 0) zeroCount++
+        }
+        return zeroCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var zeroCount = 0
+        input.forEach { rotation ->
+            val direction = rotation.take(1)
+            var distance = rotation.drop(1).toInt()
+            while (distance > 0) {
+                if (dial == 0) zeroCount++
+                if (dial == 0 && direction == "L") {
+                    dial = 99
+                    distance--
+                    continue
+                }
+                if (dial == 99 && direction == "R") {
+                    dial = 0
+                    distance--
+                    continue
+                }
+                if (direction == "R") {
+                    dial += 1
+                } else {
+                    dial -= 1
+                }
+                distance--
+            }
+        }
+        return zeroCount
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val testInput = listOf("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // Or read a large test input from the `src/Day01.txt` file:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 3)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
